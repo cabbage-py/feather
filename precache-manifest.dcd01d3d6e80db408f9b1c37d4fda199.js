a3f4c2dab77cb53c79338f6c1ec2e50c6411567b
cabbage <41481424+cabbage-py@users.noreply.github.com>
2020-04-08 17:42:24 +0800

    Add files via upload

---
+++ precache-manifest.dcd01d3d6e80db408f9b1c37d4fda199.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8ddddfd083282e42c1d6c0a1aec67cf",
+    "url": "/feather/index.html"
+  },
+  {
+    "revision": "b029617a390f60a1b7f9",
+    "url": "/feather/static/css/main.f9960cd1.chunk.css"
+  },
+  {
+    "revision": "8d66dcf5ed5ed4fc5b57",
+    "url": "/feather/static/js/2.4689d31b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/feather/static/js/2.4689d31b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b029617a390f60a1b7f9",
+    "url": "/feather/static/js/main.61d1e261.chunk.js"
+  },
+  {
+    "revision": "5c0ab359a9c1e523e1f9",
+    "url": "/feather/static/js/runtime-main.5969ad6c.js"
+  }
+]);
